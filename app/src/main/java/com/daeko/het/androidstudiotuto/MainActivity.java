bef72348e88a031150b0dfbab7233a123e01f0c1
Handy <handyinfoaiep@gmail.com>
2018-08-16 18:48:20 -0300

    Proyecto

--- app/src/main/java/com/daeko/het/androidstudiotuto/MainActivity.java
+++ app/src/main/java/com/daeko/het/androidstudiotuto/MainActivity.java
@@ -2,12 +2,21 @@ package com.daeko.het.androidstudiotuto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bTn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bTn = findViewById(R.id.button);
+
+        bTn.setOnClickListener(View );
+
     }
 }
