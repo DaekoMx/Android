c570f9cc9b9d740544826ebcdd301cdfdd5c36e4
Daeko <edenstorres@hotmail.com>
2018-08-16 15:02:05 -0700

    Anadiendo un boton

--- app/src/main/java/com/daeko/het/androidstudiotuto/MainActivity.java
+++ app/src/main/java/com/daeko/het/androidstudiotuto/MainActivity.java
@@ -2,7 +2,9 @@ package com.daeko.het.androidstudiotuto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
         bTn = findViewById(R.id.button);
 
-        bTn.setOnClickListener(View );
+        bTn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
