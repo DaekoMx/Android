8346616bc32858b88cf8d3142953ecabe04653a0
Daeko <edenstorres@hotmail.com>
2018-08-17 11:24:19 -0700

    Splash Screen y Login

--- app/src/main/java/com/daeko/het/androidstudiotuto/Main2Activity.java
+++ app/src/main/java/com/daeko/het/androidstudiotuto/Main2Activity.java
@@ -1,13 +1,83 @@
 package com.daeko.het.androidstudiotuto;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private Button bTnSalir, bTnLogin;
+    private EditText logTxt, passTxt;
+    private int counter = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //Boton de Salir
+        bTnSalir = (Button) findViewById(R.id.btnSalir);
+
+        bTnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new  AlertDialog.Builder(Main2Activity.this);
+                alertDialogBuilder.setTitle("Confirmar salida");
+                alertDialogBuilder
+                        .setMessage("¿Seguro que desea salir?")
+                        .setCancelable(false)
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Main2Activity.this.finish();
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int Which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+            }
+        }) ;
+
+        //Login
+        bTnLogin = findViewById(R.id.btnEnter);
+        logTxt = findViewById(R.id.etLogin);
+        passTxt = findViewById(R.id.etPass);
+
+        bTnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validar(logTxt.getText().toString(), passTxt.getText().toString());
+            }
+        });
+
+
     }
+
+    private void validar(String userName, String userPassword) {
+        if(userName == "Admin" && userPassword == "1234"){
+            Intent logIn = new Intent(Main2Activity.this, Menu.class);
+            startActivity(logIn);
+            finish();
+            }else{
+                counter--;
+
+                if(counter == 0){
+                    bTnLogin.setEnabled(false);
+                }
+        }
+    }
+
 }
--- app/src/main/java/com/daeko/het/androidstudiotuto/MainActivity.java
+++ app/src/main/java/com/daeko/het/androidstudiotuto/MainActivity.java
@@ -1,6 +1,8 @@
 package com.daeko.het.androidstudiotuto;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bTn;
-    Button bTnsalir;
+   private static int SPLASH_TIME_OUT = 4000;
+
 
 
 
@@ -21,48 +23,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bTn = findViewById(R.id.buttonLogin);
-
-        bTnsalir = (Button) findViewById(R.id.buttonSalir);
-
-        bTnsalir.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                AlertDialog.Builder alertDialogBuilder = new  AlertDialog.Builder(MainActivity.this);
-                alertDialogBuilder.setTitle("Confirmar salida");
-                alertDialogBuilder
-                                  .setMessage("¿Seguro que desea salir?")
-                        .setCancelable(false)
-                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                               MainActivity.this.finish();
-
-                            }
-                        })
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int Which) {
-                                dialog.cancel();
-                            }
-                        });
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
-
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(homeIntent);
+                finish();
             }
-        }) ;
+        },SPLASH_TIME_OUT);
 
 
 
 
-
-        bTn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Hola presionaste el boton", Toast.LENGTH_LONG).show();
-            }
-        });
 
     }
 
