626f38dcdabeb21e0d2628b4d0ef93880951d896
Handy <handyinfoaiep@gmail.com>
2018-08-17 10:46:21 -0300

    Proyecto

--- app/src/main/java/com/daeko/het/androidstudiotuto/MainActivity.java
+++ app/src/main/java/com/daeko/het/androidstudiotuto/MainActivity.java
@@ -1,5 +1,7 @@
 package com.daeko.het.androidstudiotuto;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,10 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button bTn;
+    Button bTnsalir;
+
+
+
 
 
     @Override
@@ -16,7 +22,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bTn = findViewById(R.id.button);
+        bTn = findViewById(R.id.buttonLogin);
+
+        bTnsalir = (Button) findViewById(R.id.buttonSalir);
+
+        bTnsalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new  AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder.setTitle("Confirmar salida");
+                alertDialogBuilder
+                                  .setMessage("¿Seguro que desea salir?")
+                        .setCancelable(false)
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                               MainActivity.this.finish();
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int Which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+            }
+        }) ;
+
+
+
+
 
         bTn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +65,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-}
+
+
+
+    }
